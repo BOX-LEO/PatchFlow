20ea574c45bb2bbce694b30c9ba001b6761fe3dd
BOX-LEO <64097091+BOX-LEO@users.noreply.github.com>
2024-03-12 18:10:14 -0400

    Add files via upload

--- train.py
+++ train.py
@@ -1,13 +1,17 @@
 import torch
+import torch.nn as nn
 import torch.optim as optim
 import time
 from loss import Loss
+from metrics import image_AUROC, pixel_AUROC
 
 
 def train_model(model, train_loader, epoch):
     optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
     criteria = Loss()
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    if torch.cuda.device_count() > 1:
+        model = nn.DataParallel(model)
     model.to(device)
     start = time.time()
     print('Training Start')
@@ -22,7 +26,13 @@ def train_model(model, train_loader, epoch):
             loss.backward()
             optimizer.step()
             train_loss += loss.item()
+
         # TODO: Add evaluation step and pick the best model
+        #  1. randomly sample image from train_loader
+        #  2. add anomaly to the image
+        #  3. calculate image_AUROC and pixel_AUROC
+        #  4. save the model if it has the best performance
+
 
         print('Epoch: {} Loss: {:.6f}'.format(i, train_loss / len(train_loader.dataset)))
     # print training time in minutes
